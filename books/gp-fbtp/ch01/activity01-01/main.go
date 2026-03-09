87f4ed59cd11efe6cd8c5d560388ed526cc4ea2e
Juan Francisco Cardona Mc'Cormick <jfcmacro@gmail.com>
2026-03-08 21:38:35 -0500

    Addicionando ejemplos Go

---
+++ books/gp-fbtp/ch01/activity01-01/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	firstName := "Bob"
+	secondName := "Smith"
+	age := 34
+	peanutAllergy := false
+	fmt.Println(firstName)
+	fmt.Println(secondName)
+	fmt.Println(age)
+	fmt.Println(peanutAllergy)
+}
