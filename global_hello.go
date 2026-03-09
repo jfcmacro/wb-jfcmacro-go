33ced0f063123f5c0cd6be494454c514da1aea4f
Juan Francisco Cardona Mc'Cormick <jfcmacro@gmail.com>
2026-03-08 21:54:56 -0500

    Adicionado README.md y .gitignore con otros ficheros

---
+++ global_hello.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	helloList := []string{
+		"Hello, world",
+		"Καλημέρα κόσμε",
+		"こんにちは世界",
+		"سلام دنیا‎",
+		"Привет, мир",
+	}
+	fmt.Println(len(helloList))
+	fmt.Println(helloList[len(helloList)-1])
+	// fmt.Println(helloList[len(helloList)])
+	for _,hello := range helloList[0:] {
+		fmt.Println(hello)
+	}
+}
